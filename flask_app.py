9df686e280b0afeb9502f108f098159d23666c0d
Alex van Winkel <alexicoo@gmail.com>
2022-03-05 14:14:32 -0500

    added route /last

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,15 @@
 import os
 import requests, json
 from flask import Flask,redirect,url_for,render_template,request
-from steam_api import get_csgo3, get_csgo4
+from steam_api import get_csgo3, get_csgo4, get_csgo5
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
     output1 = ""
     output2 = ""
+    output3 = "TEST"
 
     id = "76561198800721901"
 
@@ -23,6 +24,12 @@ def home():
     except Exception as E:
         print(E)
         output2 = "E" + str(E)
+
+    try:    
+        output3 = get_csgo5()
+    except Exception as E:
+        print(E)
+        output3 = "E" + str(E)
 
 
     if request.method=='POST':
@@ -40,9 +47,22 @@ def home():
             print(E)
             output2 = "E" + str(E)
 
-        return render_template('index.html', output1=output1, output2=output2)
-    return render_template('index.html', output1=output1, output2=output2)
+        try:
+            output3 = get_csgo5(id)
+        except Exception as E:
+            print(E)
+            output3 = "E" + str(E)
 
+        return render_template('index.html', output1=output1, output2=output2, output3=output3)
+    return render_template('index.html', output1=output1, output2=output2, output3=output3)
+
+
+@app.route('/last',methods=['GET','POST'])
+def last():
+    output1 = get_csgo5()
+    print(">>> output1", output1)
+    #output1['last_match_wins'] = "99999"
+    return render_template('last.html', output1=output1)
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
--- steam_api.py
+++ steam_api.py
@@ -7,9 +7,48 @@ def get_csgo3(id="76561198800721901"):
     result = json.loads(response.text)
     return result["response"]
 
-def get_csgo4(id=76561198800721901):
+def get_csgo4(id="76561198800721901"):
     response = requests.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=8CF76EF68B96295C7A2EB84E9C8904FB&steamid=" + id)
     result = json.loads(response.text)
     return result["playerstats"]
+
+def get_data(player, id):
+    data_dict = {}
+    try:
+        response = requests.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=8CF76EF68B96295C7A2EB84E9C8904FB&steamid=" + id)
+        result = json.loads(response.text)
+        data = result["playerstats"]["stats"]
+        for d in data:
+            name = d['name']
+            if name[:4] == "last":
+                value = d['value']
+                data_dict[player + "_" + name] = value
+    except Exception:
+        print("** ERROR ** Player probably set their account to private")
+        data_dict['error'] = "** ERROR ** " + player + " probably set their account to private"
+
+
+    return data_dict
+
+def get_csgo5():
+
+    data_dict = {}
+
+    id_alex = "76561198800721901"
+    id_devlin = "76561198176394198"
+    id_muffin = "76561198356224120"
+    id_kristiaan = "76561198049071062"
+
+    data_alex = get_data("alex", id_alex)
+    data_devlin = get_data("devlin", id_devlin)
+    data_muffin = get_data("muffin", id_muffin)
+    data_kristiaan = get_data("kristiaan", id_kristiaan)
+
+    data_dict.update(data_alex)
+    data_dict.update(data_devlin)
+    data_dict.update(data_muffin)
+    data_dict.update(data_kristiaan)
+
+    return data_dict
 
 
