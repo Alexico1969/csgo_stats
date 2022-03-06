54111ba9a256c05cff2ad487eb49df8caf5087f4
Alex van Winkel <alexicoo@gmail.com>
2022-03-05 21:56:27 -0500

    tried to fix spacing of 1 char number(2)

--- helper.py
+++ helper.py
@@ -1,10 +1,10 @@
 def fill_till(string, wanted_length):
-
-    output = string
     string_length = len(string)
     if string_length == 1:
-        string = " " + string
+        output = " " + string
         string_length = 2
+    else:
+        output = string
     spaces_to_add = wanted_length - string_length
     output += spaces_to_add * " "
     return output
